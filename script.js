f9eafdd2afc81bf2ac5dd80e94dab702ec1fc9ba
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-01 12:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
 let url = location.href;
-let regex = /[a-zA-Z]/g;
-let lenOfUrl = url.match(regex).length;
+let lenOfUrl = url.length;
 alert(`The length of the URL is: ${lenOfUrl}`);
